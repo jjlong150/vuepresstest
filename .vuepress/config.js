f0b9ed93a9731445a28a384dbaf3a6665702196f
Jeffrey Long <33353663+jjlong150@users.noreply.github.com>
2023-04-04 14:28:41 -0400

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
   themeConfig: {
     repo: "",
     editLinks: false,
-    base: "",
+    base: "/vuepresstest/",
     docsDir: "",
     editLink: false,
     editLinkText: "Edit this page",
